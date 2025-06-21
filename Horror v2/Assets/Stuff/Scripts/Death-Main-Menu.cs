6ffcd3e7e70883c296daf198b4807b3ba48fba90
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2025-06-21 17:11:17 +0500

    Main menu remake

--- Horror v2/Assets/Stuff/Scripts/Death-Main-Menu.cs
+++ Horror v2/Assets/Stuff/Scripts/Death-Main-Menu.cs
@@ -15,4 +15,68 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void B_OpenSettings()
+    {
+        Debug.Log("Settings opened!");
+        GameObject settingsPanel = GameObject.Find("SettingsPanel");
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+        }
+    }
+
+    public void B_CloseSettings()
+    {
+        Debug.Log("Settings closed!");
+        GameObject settingsPanel = GameObject.Find("SettingsPanel");
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
 }
+
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void B_LoadScene()
+    {
+        SceneManager.LoadScene("og");
+    }
+
+
+public void B_QuitGame()
+{
+    Application.Quit();
+}
+
+public void B_OpenSettings()
+{
+    Debug.Log("Settings opened!");
+    GameObject settingsPanel = GameObject.Find("SettingsPanel");
+    if (settingsPanel != null)
+    {
+        settingsPanel.SetActive(true);
+    }
+}
+
+public void B_CloseSettings()
+{
+    Debug.Log("Settings closed!");
+    GameObject settingsPanel = GameObject.Find("SettingsPanel");
+    if (settingsPanel != null)
+    {
+        settingsPanel.SetActive(false);
+    }
+}
+}
+
+*/
