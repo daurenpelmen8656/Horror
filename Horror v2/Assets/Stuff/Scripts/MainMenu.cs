d693477b6f8472655d6c2fb7866972889cb07c35
daurenpelmen8656 <155034944+daurenpelmen8656@users.noreply.github.com>
2025-06-22 19:34:27 +0500

    Main menu remake
    +Change resolution
    +FullScreen

---
+++ Horror v2/Assets/Stuff/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    public void B_LoadScene()
+    {
+        SceneManager.LoadScene("og");
+    }
+
+    public void B_QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void B_OpenSettings()
+    {
+        Debug.Log("Opening SettingsPanel...");
+        settingsPanel.SetActive(true);
+    }
+
+    public void B_CloseSettings()
+    {
+        Debug.Log("Closing SettingsPanel...");
+        settingsPanel.SetActive(false);
+    }
+}
+
---
+++ Horror v2/Assets/Stuff/Scripts/SettingMenuManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingMenuManager : MonoBehaviour
+{
+    public TMP_Dropdown ResDropDown;
+    public Toggle FullScreenToggle;
+
+    Resolution[] AllResolutions;
+    bool IsFullScreen;
+    int SelectedResolution;
+    List<Resolution> SelectedResolutionList = new List<Resolution>();
+    void Start()
+    {
+        IsFullScreen = true;
+        AllResolutions = Screen.resolutions;
+
+        List<string> resolutionStringList = new List<string>();
+        string newRes;
+        foreach (Resolution res in AllResolutions)
+        {
+            newRes = res.width.ToString() + "x" + res.height.ToString();
+            if (!resolutionStringList.Contains(newRes))
+            {
+                resolutionStringList.Add(newRes);
+                SelectedResolutionList.Add(res);
+            }
+        }
+
+        ResDropDown.AddOptions(resolutionStringList);
+    }
+
+    public void ChangeResolution()
+    {
+        SelectedResolution = ResDropDown.value;
+        Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullScreen);
+    }
+
+    public void ChangeFullScreen()
+    {
+        IsFullScreen = FullScreenToggle.isOn;
+        Screen.SetResolution(SelectedResolutionList[SelectedResolution].width, SelectedResolutionList[SelectedResolution].height, IsFullScreen);
+
+    }
+    void Update()
+    {
+        
+    }
+}
